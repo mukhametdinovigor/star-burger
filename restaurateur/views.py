d58e8005bfdf8d1c4f891b6349848a3e07507bee
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-08-15 23:34:32 +1100

    Display distance from customer address to restaurants

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,3 +1,6 @@
+import requests
+from geopy import distance
+
 from django import forms
 from django.shortcuts import redirect, render
 from django.views import View
@@ -95,6 +98,31 @@ def view_restaurants(request):
     })
 
 
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lat, lon
+
+
+def get_order_distance(restaurant_address, order_address):
+    restaurant_coords = fetch_coordinates('ee75d4fd-9401-4483-8495-d73b5a632f62', restaurant_address)
+    order_coords = fetch_coordinates('ee75d4fd-9401-4483-8495-d73b5a632f62', order_address)
+    order_distance = "{:.3f}".format(distance.distance(restaurant_coords, order_coords).km)
+    return order_distance
+
+
 def serialize_order(order):
     menu = RestaurantMenuItem.objects.select_related('restaurant')
     restaurants_in_order = []
@@ -104,7 +132,8 @@ def serialize_order(order):
         restaurants = menu.filter(product_id=product.product.id)
         for restaurant in restaurants:
             if restaurant.availability:
-                restaurants_in_product.add(restaurant.restaurant.name.split()[-1])
+                order_distance = get_order_distance(restaurant.restaurant.address, order.address)
+                restaurants_in_product.add(((restaurant.restaurant.name.split()[-1]), order_distance))
         restaurants_in_order.append(restaurants_in_product.copy())
         restaurants_in_product.clear()
     unique_restaurants = restaurants_in_order[0]
@@ -116,6 +145,7 @@ def serialize_order(order):
         'id': order.id,
         'status': order.status,
         'restaurants': restaurants_in_order,
+
         'payment_method': order.payment_method,
         'cost': order.cost,
         'fullname': f'{order.firstname} {order.lastname}',
@@ -127,9 +157,16 @@ def serialize_order(order):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
+    # restaurant_address = Restaurant.objects.all().last().address
+    # order_address = CustomerOrderDetails.objects.all().last().address
+    # restaurant_coords = fetch_coordinates('ee75d4fd-9401-4483-8495-d73b5a632f62', restaurant_address)
+    # order_coords = fetch_coordinates('ee75d4fd-9401-4483-8495-d73b5a632f62', order_address)
+    # dist = "{:.3f}".format(distance.distance(restaurant_coords, order_coords).km)
+
     order_items = CustomerOrderDetails.objects.get_order_with_cost().prefetch_related('order_items__product')
     context = {
-        'order_items': [serialize_order(order) for order in order_items]
+        'order_items': [serialize_order(order) for order in order_items],
+        # 'dist': dist
     }
 
     return render(request, template_name='order_items.html', context=context)
